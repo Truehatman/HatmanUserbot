d69f69f833badf89b2770ed980b2008f6387f274
hatmanexchange <156363293+hatmanexchange@users.noreply.github.com>
2024-01-24 23:17:03 +0100

    Update HatmanUserbot.py

--- HatmanUserbot.py
+++ HatmanUserbot.py
@@ -392,13 +392,15 @@ async def mute_user(_, message):
         print(f"Error while muting user: {e}")
         await message.edit_text("Error while muting user.")
 
-@ubot.on_message(filters.user(self) & ~filters.private)
-async def delete_messages(_, message):
+@ubot.on_message(filters.text)
+async def delete_muted_messages(client, message):
     try:
-        if is_user_muted(message.from_user.id):
+        # Verifica se l'utente è muteato e se il mittente è valido
+        if message.from_user and is_user_muted(message.from_user.id):
+            # Elimina il messaggio
             await message.delete()
     except Exception as e:
-        print(f"Error while deleting message: {e}")
+        print(f"Error while deleting muted user's message: {e}")
 
 @ubot.on_message(filters.command("unmute", ".") & filters.reply)
 async def unmute_user(_, message):
